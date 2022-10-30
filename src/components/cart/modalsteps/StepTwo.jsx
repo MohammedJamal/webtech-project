bc19590a1872982cde11987236957b2342ae28dd
Supakorn Netsuwan <earth_killerdark@hotmail.com>
2022-10-30 20:15:05 +0700

    Update : payment modal 2

--- src/components/cart/modalsteps/StepTwo.jsx
+++ src/components/cart/modalsteps/StepTwo.jsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useContext } from "react";
 import { CartContext } from "../../../context/CartContext";
 import ModalNavigate from "../ModalNavigate";
+import TextField from "@mui/material/TextField";
+
+const FormWrapper = ({ children }) => (
+  <form className="w-2/3 mt-4">
+    {children}
+  </form>
+)
 
 const StepTwo = () => {
   const [cartState, setCartState] = useContext(CartContext);
@@ -10,10 +17,41 @@ const StepTwo = () => {
 
   return (
     <div className=" min-h-full flex justify-between flex-col">
-      <p>Hello, {creditCard.name}</p>
+      <div>
+        <h2 className="text-bluegrey-dark-2">ข้อมูลบัตร Visa</h2>
+        <FormWrapper>
+          <div className="grid grid-cols-12 gap-3">
+            <TextField id="cardholder-name" label="Full name" variant="outlined" className="col-span-12"/>
+            <TextField id="card-number" label="Card number" variant="outlined" className="col-span-6"/>
+            <TextField id="card-cvc" label="CVC" variant="outlined" className="col-span-3"/>
+            <TextField id="card-expire" label="Expire" variant="outlined" className="col-span-3"/>
+          </div>
+        </FormWrapper>
+        <FormWrapper>
+          <h2 className="text-bluegrey-dark-2">ข้อมูลใบเสร็จ</h2>
+          <div className="grid grid-cols-12 gap-3">
+            <TextField id="firstname" label="First name" variant="outlined" className="col-span-6"/>
+            <TextField id="lastname" label="Last name" variant="outlined" className="col-span-6"/>
+            <TextField id="email" label="Email" variant="outlined" className="col-span-6"/>
+            <TextField id="phonenumber" label="Phone number" variant="outlined" className="col-span-6"/>
+            <>
+              <TextField id="address-line-1" label="Address line 1" variant="outlined" className="w-full"/>
+              <TextField id="address-line-2" label="Address line 2" variant="outlined" className="w-full"/>
+              <>
+                <TextField id="country" label="ฉountry" variant="outlined" className="w-full"/>
+                <TextField id="province-state" label="Province/State" variant="outlined" className="w-full"/>
+                <TextField id="zipcode" label="Zipcode" variant="outlined" className="w-full"/>
+              </>
+            </>
+          </div>
+        </FormWrapper>
+      </div>
+
       <ModalNavigate>
         <div className="flex justify-between">
-          <button onClick={goBack} className="text-dark-1 px-2">ย้อนกลับ</button>
+          <button onClick={goBack} className="text-dark-1 px-2">
+            ย้อนกลับ
+          </button>
           <button className="text-blue-500 px-2">ถัดไป</button>
         </div>
       </ModalNavigate>
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 export const CartContext = React.createContext();
 
-const CartProvider = ({ children }) => {
+/*const CartProvider = ({ children }) => {
   const [cartState, setCartState] = useState({
     creditCard: {
       name: null,
@@ -28,6 +28,53 @@ const CartProvider = ({ children }) => {
       }));
     },
     paymentStep: 1,
+    setPaymentStep: (step) => {
+      if (step > 3) return; // prevent voerstep bug.
+      setCartState((prevCartState) => ({
+        ...prevCartState,
+        paymentStep: step,
+      }));
+    },
+  });
+
+  React.useEffect(() => {
+    console.log(cartState);
+  });
+
+  return (
+    <CartContext.Provider value={[cartState, setCartState]}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+*/
+
+const CartProvider = ({ children }) => {
+  const [cartState, setCartState] = useState({
+    creditCard: {
+      name: "Mastercard",
+      imgURL: "/mastercard.svg",
+    },
+    setCreditCard: (card) => {
+      setCartState((prevCartState) => ({
+        ...prevCartState,
+        creditCard: card,
+        paymentStep: 2,
+      }));
+    },
+    isModalOpen: true,
+    setIsModalOpen: (state) => {
+      if (state === false) {
+        // reset payment
+        cartState.setPaymentStep(1);
+      }
+
+      setCartState((prevCartState) => ({
+        ...prevCartState,
+        isModalOpen: state,
+      }));
+    },
+    paymentStep: 2,
     setPaymentStep: (step) => {
       if (step > 3) return; // prevent voerstep bug.
       setCartState((prevCartState) => ({
