0e594383cba3e7ad881d02106dbc264993908b81
Supakorn Netsuwan <72784696+SupakornNetsuwan@users.noreply.github.com>
2022-11-03 22:25:47 +0700

    update : payment page done

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -52,8 +52,8 @@ export const CartContext = React.createContext();
 const CartProvider = ({ children }) => {
   const [cartState, setCartState] = useState({
     creditCard: {
-      name: "Mastercard",
-      imgURL: "/mastercard.svg",
+      // name: "Mastercard",
+      // imgURL: "/mastercard.svg",
     },
     setCreditCard: (card) => {
       setCartState((prevCartState) => ({
@@ -62,7 +62,7 @@ const CartProvider = ({ children }) => {
         paymentStep: 2,
       }));
     },
-    isModalOpen: true,
+    isModalOpen: false,
     setIsModalOpen: (state) => {
       if (state === false) {
         // reset payment
@@ -74,12 +74,34 @@ const CartProvider = ({ children }) => {
         isModalOpen: state,
       }));
     },
-    paymentStep: 2,
+    paymentStep: 1,
     setPaymentStep: (step) => {
       if (step > 3) return; // prevent voerstep bug.
       setCartState((prevCartState) => ({
         ...prevCartState,
         paymentStep: step,
+      }));
+    },
+    purchaseDetail: {
+      // address1: "99/924  C/24  หมูบ้านพาร์คเวย์โฮม  ถนนรามคำแหง 150",
+      // address2: "เขตสะพานสูง  แขวงสะพานสูง  กทม.10240",
+      // cardNumber: "asdasd",
+      // cardholderName: "asdas",
+      // country: "Thailand",
+      // cvc: "123",
+      // email: "earth_killerdark@hotmail.com",
+      // expire: "ascd",
+      // firstname: "ศุภกร",
+      // lastname: "เนตรสุวรรณ",
+      // phonenumber: "+66959259515",
+      // provinceState: "กรุงเทพมหานคร",
+      // zipCode: "10240",
+    },
+    setPurchaseDetail: (detail) => {
+      setCartState((prevCartState) => ({
+        ...prevCartState,
+        purchaseDetail: detail,
+        paymentStep: 3, // It's now step 2 so next step is 3
       }));
     },
   });
