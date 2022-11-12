58ce53d89a58c7c1a5810c42a8e9a589f9ba3706
Supakorn Netsuwan <earth_killerdark@hotmail.com>
2022-11-12 21:48:51 +0700

    Fix : Trying to fix CarDetail importing

--- src/components/car/tabs/Detail.jsx
+++ src/components/car/tabs/Detail.jsx
@@ -2,35 +2,40 @@ import React, { useContext } from "react";
 /* ============ context ============ */
 import { CarDetailContext } from "../../../context/CarDetailContext";
 
-const CarDetail = () => {
+const Detail = () => {
   const carDetail = useContext(CarDetailContext);
-  
+  console.log(carDetail)
   const listDisplay = [
     {
-      name:"Acceleration",
+      name: "Acceleration",
       value: carDetail.Acceleration,
-    },{
+    },
+    {
       name: "Wheelbase",
       value: carDetail.Wheelbase,
-    },{
+    },
+    {
       name: "Combined",
       value: carDetail.combined,
-    },{
+    },
+    {
       name: "powerKW",
       value: carDetail.powerKW,
-    },{
+    },
+    {
       name: "powerPS",
-      value: carDetail.powerPS
-    },{
+      value: carDetail.powerPS,
+    },
+    {
       name: "topspeed",
       value: carDetail.topspeed,
-    }
-  ]
+    },
+  ];
   return (
     <>
       <div className="">
         <div className="w-full h-96 bg-lighten-2 rounded" />
-        <Detail>
+        <div className="mt-8 ">
           <h2>
             <span className="py-0 px-2   bg-blue-50 rounded-lg text-blue-500">
               {carDetail.carbrand[0].toUpperCase() +
@@ -39,21 +44,21 @@ const CarDetail = () => {
             <span className="text-bluegrey-dark-2"> {carDetail.name}</span>
           </h2>
           <ul className="mt-4 bg-lighten-3/20 p-2 rounded divide-y">
-            {listDisplay.map(detail => {
+            {listDisplay.map((detail) => {
               return (
                 <li key={detail.name} className="flex justify-between py-2">
-                  <p className="text-bluegrey-dark-2 font-medium">{detail.name}</p>
+                  <p className="text-bluegrey-dark-2 font-medium">
+                    {detail.name}
+                  </p>
                   <p className="text-blue-500">{detail.value}</p>
                 </li>
-              )
+              );
             })}
           </ul>
-        </Detail>
+        </div>
       </div>
     </>
   );
 };
 
-const Detail = ({ children }) => <div className="mt-8 ">{children}</div>;
-
-export default CarDetail;
+export default Detail;
