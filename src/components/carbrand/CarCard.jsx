53982d2ce9f71f79e536acd87c47e49df99bc824
Supakorn Netsuwan <earth_killerdark@hotmail.com>
2022-11-12 15:36:30 +0700

    Update  : Add animation on hover

--- src/components/carbrand/CarCard.jsx
+++ src/components/carbrand/CarCard.jsx
@@ -20,9 +20,9 @@ const CarCard = (props) => {
   };
 
   return (
-    <div onClick={navigateToCar} className="cursour">
-      <div className="w-full aspect-square bg-lighten-3 rounded">
-        <img src={imageURL + "/" + imgFileType} alt={name} className="w-full h-full object-cover"/>
+    <div onClick={navigateToCar} className="cursor-pointer group">
+      <div className="w-full aspect-square bg-lighten-3 rounded overflow-hidden">
+        <img src={imageURL + "/" + imgFileType} alt={name} className="group-hover:scale-[1.1] w-full h-full object-cover transition-all duration-150 ease-out"/>
       </div>
       <div className="py-2">
         <h4 className="text-lg text-dark-4">{props.name}</h4>
